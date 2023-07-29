6ecdcfb99197c0297e5d56fa5786b71e5e0088dc
DustyBoats <128747023+DustyBoats@users.noreply.github.com>
2023-07-29 13:52:14 -0600

    Add files via upload

--- plugin.js
+++ plugin.js
@@ -3,81 +3,96 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-/**
- * @fileOverview Special Character plugin
- */
-
-CKEDITOR.plugins.add( 'specialchar', {
-	// List of available localizations.
-	// jscs:disable
-	availableLangs: { af:1,ar:1,az:1,bg:1,ca:1,cs:1,cy:1,da:1,de:1,'de-ch':1,el:1,en:1,'en-au':1,'en-ca':1,'en-gb':1,eo:1,es:1,'es-mx':1,et:1,eu:1,fa:1,fi:1,fr:1,'fr-ca':1,gl:1,he:1,hr:1,hu:1,id:1,it:1,ja:1,km:1,ko:1,ku:1,lt:1,lv:1,nb:1,nl:1,no:1,oc:1,pl:1,pt:1,'pt-br':1,ro:1,ru:1,si:1,sk:1,sl:1,sq:1,sr:1,'sr-latn':1,sv:1,th:1,tr:1,tt:1,ug:1,uk:1,vi:1,zh:1,'zh-cn':1 },
-	lang: 'af,ar,az,bg,bn,bs,ca,cs,cy,da,de,de-ch,el,en,en-au,en-ca,en-gb,eo,es,es-mx,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,oc,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,tt,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
-	// jscs:enable
+CKEDITOR.plugins.add( 'smiley', {
 	requires: 'dialog',
-	icons: 'specialchar', // %REMOVE_LINE_CORE%
+	// jscs:disable maximumLineLength
+	lang: 'af,ar,az,bg,bn,bs,ca,cs,cy,da,de,de-ch,el,en,en-au,en-ca,en-gb,eo,es,es-mx,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,oc,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,tt,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
+	// jscs:enable maximumLineLength
+	icons: 'smiley', // %REMOVE_LINE_CORE%
 	hidpi: true, // %REMOVE_LINE_CORE%
 	init: function( editor ) {
-		var pluginName = 'specialchar',
-			plugin = this;
-
-		// Register the dialog.
-		CKEDITOR.dialog.add( pluginName, this.path + 'dialogs/specialchar.js' );
-
-		editor.addCommand( pluginName, {
-			exec: function() {
-				var langCode = editor.langCode;
-				langCode =
-					plugin.availableLangs[ langCode ] ? langCode :
-					plugin.availableLangs[ langCode.replace( /-.*/, '' ) ] ? langCode.replace( /-.*/, '' ) :
-					'en';
-
-				CKEDITOR.scriptLoader.load( CKEDITOR.getUrl( plugin.path + 'dialogs/lang/' + langCode + '.js' ), function() {
-					CKEDITOR.tools.extend( editor.lang.specialchar, plugin.langEntries[ langCode ] );
-					editor.openDialog( pluginName );
-				} );
-			},
-			modes: { wysiwyg: 1 },
-			canUndo: false
-		} );
-
-		// Register the toolbar button.
-		editor.ui.addButton && editor.ui.addButton( 'SpecialChar', {
-			label: editor.lang.specialchar.toolbar,
-			command: pluginName,
+		editor.config.smiley_path = editor.config.smiley_path || ( this.path + 'images/' );
+		editor.addCommand( 'smiley', new CKEDITOR.dialogCommand( 'smiley', {
+			allowedContent: 'img[alt,height,!src,title,width]',
+			requiredContent: 'img'
+		} ) );
+		editor.ui.addButton && editor.ui.addButton( 'Smiley', {
+			label: editor.lang.smiley.toolbar,
+			command: 'smiley',
 			toolbar: 'insert,50'
 		} );
+		CKEDITOR.dialog.add( 'smiley', this.path + 'dialogs/smiley.js' );
 	}
 } );
 
 /**
- * The list of special characters visible in the "Special Character" dialog window.
+ * The base path used to build the URL for the smiley images. It must end with a slash.
  *
- *		config.specialChars = [ '&quot;', '&rsquo;', [ '&custom;', 'Custom label' ] ];
- *		config.specialChars = config.specialChars.concat( [ '&quot;', [ '&rsquo;', 'Custom label' ] ] );
+ *		config.smiley_path = 'http://www.example.com/images/smileys/';
+ *
+ *		config.smiley_path = '/images/smileys/';
+ *
+ * @cfg {String} [smiley_path=CKEDITOR.basePath + 'plugins/smiley/images/']
+ * @member CKEDITOR.config
+ */
+
+/**
+ * The file names for the smileys to be displayed. These files must be
+ * contained inside the URL path defined with the {@link #smiley_path} setting.
+ *
+ *		// This is actually the default value.
+ *		config.smiley_images = [
+ *			'regular_smile.png','sad_smile.png','wink_smile.png','teeth_smile.png','confused_smile.png','tongue_smile.png',
+ *			'embarrassed_smile.png','omg_smile.png','whatchutalkingabout_smile.png','angry_smile.png','angel_smile.png','shades_smile.png',
+ *			'devil_smile.png','cry_smile.png','lightbulb.png','thumbs_down.png','thumbs_up.png','heart.png',
+ *			'broken_heart.png','kiss.png','envelope.png'
+ *		];
  *
  * @cfg
  * @member CKEDITOR.config
  */
-CKEDITOR.config.specialChars = [
-	'!', '&quot;', '#', '$', '%', '&amp;', "'", '(', ')', '*', '+', '-', '.', '/',
-	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';',
-	'&lt;', '=', '&gt;', '?', '@',
-	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-	'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-	'[', ']', '^', '_', '`',
-	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-	'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-	'{', '|', '}', '~',
-	'&euro;', '&lsquo;', '&rsquo;', '&ldquo;', '&rdquo;', '&ndash;', '&mdash;', '&iexcl;', '&cent;', '&pound;',
-	'&curren;', '&yen;', '&brvbar;', '&sect;', '&uml;', '&copy;', '&ordf;', '&laquo;', '&not;', '&reg;', '&macr;',
-	'&deg;', '&sup2;', '&sup3;', '&acute;', '&micro;', '&para;', '&middot;', '&cedil;', '&sup1;', '&ordm;', '&raquo;',
-	'&frac14;', '&frac12;', '&frac34;', '&iquest;', '&Agrave;', '&Aacute;', '&Acirc;', '&Atilde;', '&Auml;', '&Aring;',
-	'&AElig;', '&Ccedil;', '&Egrave;', '&Eacute;', '&Ecirc;', '&Euml;', '&Igrave;', '&Iacute;', '&Icirc;', '&Iuml;',
-	'&ETH;', '&Ntilde;', '&Ograve;', '&Oacute;', '&Ocirc;', '&Otilde;', '&Ouml;', '&times;', '&Oslash;', '&Ugrave;',
-	'&Uacute;', '&Ucirc;', '&Uuml;', '&Yacute;', '&THORN;', '&szlig;', '&agrave;', '&aacute;', '&acirc;', '&atilde;',
-	'&auml;', '&aring;', '&aelig;', '&ccedil;', '&egrave;', '&eacute;', '&ecirc;', '&euml;', '&igrave;', '&iacute;',
-	'&icirc;', '&iuml;', '&eth;', '&ntilde;', '&ograve;', '&oacute;', '&ocirc;', '&otilde;', '&ouml;', '&divide;',
-	'&oslash;', '&ugrave;', '&uacute;', '&ucirc;', '&uuml;', '&yacute;', '&thorn;', '&yuml;', '&OElig;', '&oelig;',
-	'&#372;', '&#374', '&#373', '&#375;', '&sbquo;', '&#8219;', '&bdquo;', '&hellip;', '&trade;', '&#9658;', '&bull;',
-	'&rarr;', '&rArr;', '&hArr;', '&diams;', '&asymp;'
+CKEDITOR.config.smiley_images = [
+	'regular_smile.png', 'sad_smile.png', 'wink_smile.png', 'teeth_smile.png', 'confused_smile.png', 'tongue_smile.png',
+	'embarrassed_smile.png', 'omg_smile.png', 'whatchutalkingabout_smile.png', 'angry_smile.png', 'angel_smile.png', 'shades_smile.png',
+	'devil_smile.png', 'cry_smile.png', 'lightbulb.png', 'thumbs_down.png', 'thumbs_up.png', 'heart.png',
+	'broken_heart.png', 'kiss.png', 'envelope.png'
 ];
+
+/**
+ * The description to be used for each of the smileys defined in the
+ * {@link CKEDITOR.config#smiley_images} setting. Each entry in this array list
+ * must match its relative pair in the {@link CKEDITOR.config#smiley_images}
+ * setting.
+ *
+ *		// Default settings.
+ *		config.smiley_descriptions = [
+ *			'smiley', 'sad', 'wink', 'laugh', 'frown', 'cheeky', 'blush', 'surprise',
+ *			'indecision', 'angry', 'angel', 'cool', 'devil', 'crying', 'enlightened', 'no',
+ *			'yes', 'heart', 'broken heart', 'kiss', 'mail'
+ *		];
+ *
+ *		// Use textual emoticons as description.
+ *		config.smiley_descriptions = [
+ *			':)', ':(', ';)', ':D', ':/', ':P', ':*)', ':-o',
+ *			':|', '>:(', 'o:)', '8-)', '>:-)', ';(', '', '', '',
+ *			'', '', ':-*', ''
+ *		];
+ *
+ * @cfg
+ * @member CKEDITOR.config
+ */
+CKEDITOR.config.smiley_descriptions = [
+	'smiley', 'sad', 'wink', 'laugh', 'frown', 'cheeky', 'blush', 'surprise',
+	'indecision', 'angry', 'angel', 'cool', 'devil', 'crying', 'enlightened', 'no',
+	'yes', 'heart', 'broken heart', 'kiss', 'mail'
+];
+
+/**
+ * The number of columns to be generated by the smilies matrix.
+ *
+ *		config.smiley_columns = 6;
+ *
+ * @since 3.3.2
+ * @cfg {Number} [smiley_columns=8]
+ * @member CKEDITOR.config
+ */
